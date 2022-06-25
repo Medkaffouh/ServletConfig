46ba8c3ca025adbcc5e8567f976b549a2ec955e2
Medkaffouh <kaffouhmohamed@gmail.com>
2022-06-25 22:39:24 +0100

    She wants a ServletContextListener

---
+++ src/main/java/com/example/servletconfig/Dog.java
@@ -0,0 +1,11 @@
+package com.example.servletconfig;
+
+public class Dog {
+    private String breed;
+    public Dog(String breed) {
+        this.breed = breed;
+    }
+    public String getBreed() {
+        return breed;
+    }
+}
